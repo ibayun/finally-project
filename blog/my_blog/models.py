335b0ce7ddf323826867d066d07d18b7af2ae089
ibayun <i.korniyenko@list.ru>
2019-10-04 23:26:33 +0300

    added tags, crete new slug

--- blog/my_blog/models.py
+++ blog/my_blog/models.py
@@ -1,17 +1,40 @@
 from django.db import models
 from django.shortcuts import reverse
-# Create your models here.
+from time import time
+from django.utils.text import slugify
+
+
+def generate_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
 
 
 class Post(models.Model):
     objects = None
     article_title = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
     article_text = models.TextField(blank=True, db_index=True)
     article_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = generate_slug(self.article_title)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.article_title
+
+
+class Tag(models.Model):
+    tag_title = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=40, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return '{}'.format(self.tag_title)
--- blog/my_blog/views.py
+++ blog/my_blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Post
+from django.views import View
+
+from .models import Post, Tag
 
 # Create your views here.
 
@@ -12,3 +14,21 @@ def posts_article(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'network/post_detail.html', context={'post': post})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'network/tagsList.html', context={'tags': tags})
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'network/tagDetail.html', context={'tag': tag})
+
+
+class post_update(View):
+    def get(self, request, slug):
+        tag = Tag.object.get(slug__iexact=slug)
+        bound_form = TagForm(instance=tag)
+        return render(request, 'network/post_update.html', context={'form': bound_form, 'tag': tag})
+
